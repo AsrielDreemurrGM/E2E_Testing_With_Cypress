2b819000f157a0f36d60e039fc4a0f357c7aac04
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-02-22 01:04:27 -0300

    Criado Testes Para a Página de Candidatura
    - Criado Teste de Navegação Para a Página e de Preenchimento e Envio do Formulário;
    - BeforeEach Adicionado Para Acessar o Site Antes de Todo Teste Sem Repetir o Código;

---
+++ cypress/e2e/ebac-jobs/candidatura.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Testes para a página de candidatura', () => {
+    beforeEach(() => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app/');
+    })
+
+    it('Deve levar o usuário para o formulário de inscrição', () => {
+        cy.get('.Vaga_vagaLink__DeFkk').first().click();
+
+        cy.get('input').should('have.length', 7);
+    })
+    it('Deve prencher o formulário de inscrição e enviá-lo', () => {
+        cy.get('.Vaga_vagaLink__DeFkk').first().click();
+
+        cy.get('input[name="nome-completo"]').type('Eduardo Augusto');
+        cy.get('input[name="email"]').type('EduardoAugusto@teste.com');
+        cy.get('input[name="telefone"]').type('49 12121212');
+        cy.get('input[name="endereco"]').type('Rua Testes, Bairro Cypress, WebInternet-COM');
+        cy.get('select[name="escolaridade"]').select('medio-completo');
+        cy.get('#mac').check();
+        cy.get('.Aplicacao_button__tw2AE').click();
+
+        cy.on('window:alert', (conteudo) => {
+            expect(conteudo).contain('Obrigado pela candidatura!')
+        })
+    })
+})
